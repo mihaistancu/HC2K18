be4d1aa39dabe62e34385b77f09cca1b8af2eaa5
Adrian Barglazan <jm_aba@yahoo.com>
2018-03-01 20:51:08 +0200

    df

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,21 +34,65 @@ namespace ConsoleApp2
         public int T { get; set; }
     }
 
+    public class OutputData
+    {
+        public List<List<int>> Rides { get; set; }
+        public OutputData()
+        {
+            Rides = new List<List<int>>();
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            InputData inputData = ReadInputFile(@"C:\Users\AdiB\Downloads\a_example.in");
-            Console.Write(inputData.R + " " + inputData.C)
+            InputData inputData = ReadInputFile(@"a_example.in");
+            Console.Write(inputData.R + " " + inputData.C + "  " + inputData.F + " " + inputData.N + " " + inputData.B + " " + inputData.T);
+            Console.WriteLine();
+            for (int i = 0; i < inputData.N; i++)
+            {
+                Console.Write(inputData.Rides[i].A + " " + inputData.Rides[i].B + " " + inputData.Rides[i].X + " " + inputData.Rides[i].Y + " " + inputData.Rides[i].S + " " + inputData.Rides[i].T);
+                Console.WriteLine();
 
+            }
+
+            var outputData = new OutputData();
+
+            outputData.Rides.Add(new List<int> { 1, 0 });
+            outputData.Rides.Add(new List<int> { 2, 1 , 2 });
+
+            GenerateFile(outputData,"out.txt");
 
             Console.ReadLine();
+        }
+
+        private static void GenerateFile(OutputData outputData, string filePath)
+        {
+
+            var file = new System.IO.StreamWriter(filePath);
+            file.NewLine = "\n";
+            foreach (var item in outputData.Rides)
+            {
+                StringBuilder s = new StringBuilder();
+                foreach (var i in item)
+                {
+                    s.Append(i + " ");
+                }
+
+                file.WriteLine(s.ToString());
+            }
+            file.Flush();
+            file.Close();
         }
 
         private static InputData ReadInputFile(string filePAth)
         {
             var inputData = new InputData();
-
+            var s = Directory.GetCurrentDirectory();
+            filePAth = Path.Combine(s, filePAth);
             string line = string.Empty;
             var file = new System.IO.StreamReader(filePAth);
             var firstLine = file.ReadLine().Split(new[] { ' ' });
@@ -57,7 +102,7 @@ namespace ConsoleApp2
             inputData.N = Convert.ToInt32(firstLine[3]);
             inputData.B = Convert.ToInt32(firstLine[4]);
             inputData.T = Convert.ToInt32(firstLine[5]);
-            for (int i = 0; i < inputData.F; i++)
+            for (int i = 0; i < inputData.N; i++)
             {
                 var l = file.ReadLine().Split(new[] { ' ' });
 
