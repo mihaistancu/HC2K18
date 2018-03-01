0e1b7f2e6b9ed3859862d1112582977af52270f4
jmaba <jm_aba@yahoo.com>
2018-03-01 22:43:59 +0200

    mama

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using GeneticSharp.Domain;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Fitnesses;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Randomizations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Domain.Terminations;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -41,14 +50,24 @@ namespace ConsoleApp2
         {
             Rides = new List<List<int>>();
         }
+
+        public OutputData(int numberOfCars)
+        {
+            Rides = new List<List<int>>(numberOfCars);
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                Rides[i] = new List<int>();
+            }
+        }
     }
 
 
 
     class Program
     {
-        static void Main(string[] args)  
-        {  
+        static void Main(string[] args)
+        {
             InputData inputData = ReadInputFile(@"a_example.in");
             Console.Write(inputData.R + " " + inputData.C + "  " + inputData.F + " " + inputData.N + " " + inputData.B + " " + inputData.T);
             Console.WriteLine();
@@ -62,9 +81,9 @@ namespace ConsoleApp2
             var outputData = new OutputData();
 
             outputData.Rides.Add(new List<int> { 1, 0 });
-            outputData.Rides.Add(new List<int> { 2, 1 , 2 });
+            outputData.Rides.Add(new List<int> { 2, 1, 2 });
 
-            GenerateFile(outputData,"out.txt");
+            GenerateFile(outputData, "out.txt");
 
             Console.ReadLine();
         }
@@ -106,7 +125,8 @@ namespace ConsoleApp2
             {
                 var l = file.ReadLine().Split(new[] { ' ' });
 
-                inputData.Rides.Add(new Ride() {
+                inputData.Rides.Add(new Ride()
+                {
                     A = Convert.ToInt32(l[0]),
                     B = Convert.ToInt32(l[1]),
                     X = Convert.ToInt32(l[2]),
@@ -117,6 +137,80 @@ namespace ConsoleApp2
             }
 
             return inputData;
+        }
+    }
+
+    public class MyProblemFitness : IFitness
+    {
+        private InputData input;
+        public MyProblemFitness(InputData input)
+        {
+            this.input = input;
+        }
+        public double Evaluate(IChromosome chromosome)
+        {
+            var myC = (MyProblemChromosome)chromosome;
+            var genes = myC.GetGenes();
+
+            OutputData data = new OutputData(input.F);
+            for (int i = 0; i< genes.Length;i++)
+            {
+                var car = Convert.ToInt32(genes[i].Value);
+                data.Rides[car].Add(i);
+            }
+            
+            return new Score(input, data).Compute();
+        }
+    }
+    
+    public class MyProblemChromosome : ChromosomeBase
+    {
+        // Change the argument value passed to base construtor to change the length 
+        // of your chromosome.
+        private int numberOfRides;
+        private int numberOfCars;
+        public MyProblemChromosome(int numberOfCars, int numberOfRides) : base(numberOfRides)
+        {
+            this.numberOfRides = numberOfRides;
+            this.numberOfCars = numberOfCars;
+
+            var citiesIndexes = RandomizationProvider.Current.GetInts(numberOfRides, 0, numberOfCars);
+
+            for (int i = 0; i < numberOfRides; i++)
+            {
+                ReplaceGene(i, new Gene(citiesIndexes[i]));
+            }
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            return new Gene(RandomizationProvider.Current.GetInt(0, numberOfCars));
+        }
+
+        public override IChromosome CreateNew()
+        {
+            return new MyProblemChromosome(numberOfCars, numberOfRides);
+        }
+    }
+
+    class Test
+    {
+        public void DoIt(InputData inputData)
+        {
+            var selection = new EliteSelection();
+            var crossover = new OrderedCrossover();
+            var mutation = new ReverseSequenceMutation();
+            var fitness = new MyProblemFitness(inputData);
+            var chromosome = new MyProblemChromosome(inputData.F,inputData.N);
+            var population = new Population(50, 70, chromosome);
+
+            var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+            ga.Termination = new GenerationNumberTermination(100);
+
+            Console.WriteLine("GA running...");
+            ga.Start();
+
+            Console.WriteLine("Best solution found has {0} fitness.", ga.BestChromosome.Fitness);
         }
     }
 }
